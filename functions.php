96b4d8d2d7ddc75830e6cba3e4f032765b3fc558
Mattia Basaglia <mattia.basaglia@gmail.com>
2015-01-20 12:57:25 +0100

    Hide private menus

--- functions.php
+++ functions.php
@@ -1,11 +1,10 @@
 <?php
 
-$content_width = null;
-
-
 add_action( 'wp_enqueue_scripts', 'eactheme_enqueue_scripts' );
 add_action( 'widgets_init', 'eactheme_widgets_init' );
 add_action( 'wp_head', 'eactheme_head' );
+
+add_filter( 'wp_nav_menu_objects', 'eactheme_filter_menu');
 
 function eactheme_enqueue_scripts()
 {
@@ -28,4 +27,25 @@ function eactheme_head()
 {
 	echo '<link rel="icon" type="image/png" href="'.
 		get_stylesheet_directory_uri().'/img/eacon.png" />';
+}
+
+// Removes private pages (and their children) from menus
+function eactheme_filter_menu( $sorted_menu_items )
+{
+	if ( in_array( 'administrator', (array) wp_get_current_user()->roles ) )
+		return $sorted_menu_items;
+		
+	$output = array();
+	$menuitem_is_private = array();
+	foreach ( $sorted_menu_items as $item )
+	{
+		$page = WP_Post::get_instance($item->object_id);
+		if ( ($page && $page->post_status == 'private') ||
+			 ($item->menu_item_parent && $menuitem_is_private[$item->menu_item_parent]) )
+			$menuitem_is_private[$item->ID] = true;
+		else
+			$output []= $item;
+	}
+	
+	return $output;
 }
